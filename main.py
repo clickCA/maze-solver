82e013534f9b59782063c03339f7ffcacb2765f9
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-12-12 16:39:25 +0700

    add bfs search

--- main.py
+++ main.py
@@ -2,6 +2,35 @@ import tkinter as tk
 from tkinter import messagebox
 import time
 from generate_maze import generate_maze
+from collections import deque
+
+def bfs(maze, start, end):
+    rows, cols = len(maze), len(maze[0])
+    print("END", end)
+    def is_valid(x, y):
+        return 0 <= x < rows and 0 <= y < cols and maze[x][y] == 'c'
+
+    def explore(x, y):
+        
+      
+        directions = [(0, 1),(1, 0),(-1, 0), (0, -1)]
+        queue = deque([(x, y)])
+        while queue:
+            curr_x, curr_y = queue.popleft()
+            print(curr_x, curr_y)
+            for dx, dy in directions:
+                nx, ny = curr_x + dx, curr_y + dy
+                if is_valid(nx, ny) and maze[nx][ny] != 'v':
+                    if (nx, ny) == end:
+                        return True  # Found a path to the destination
+                    maze[nx][ny] = 'v'  # Mark the cell as visited
+                    queue.append((nx, ny))
+            update_canvas()
+            time.sleep(0.05)  # Adjust the delay as needed
+        return False  # No path found from this cell
+
+    start_x, start_y = start
+    return explore(start_x, start_y)
 def dfs(maze, start, end):
     rows, cols = len(maze), len(maze[0])
     print("END", end)
@@ -50,11 +79,16 @@ maze = generate_maze(height,width)
 start_position = (0, 1)
 end_position = (height-1 ,width-2)
 def solve_maze():
-    if dfs(maze, start_position, end_position):
+    if (bfs(maze, start_position, end_position)):
         print("Path found!")
         maze[end_position[0]][end_position[1]] = 'v'
         update_canvas()
         messagebox.showinfo("Maze Solver", "Path Found!")
+    # if dfs(maze, start_position, end_position):
+    #     print("Path found!")
+    #     maze[end_position[0]][end_position[1]] = 'v'
+    #     update_canvas()
+    #     messagebox.showinfo("Maze Solver", "Path Found!")
     else:
         print("No path found.")
 # Tkinter setup
